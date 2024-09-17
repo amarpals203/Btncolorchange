86dc9340738fd09c36f76bfb78b6f1492cb20cad
Amarpal Singh <113790679+amarpals203@users.noreply.github.com>
2024-09-17 11:11:37 +0530

    Add files via upload

---
+++ btn.js
@@ -0,0 +1,33 @@
+// let result1 =document.getElementsByClassName("btn1")
+ btncolor1 =(color)=>
+{
+    document.body.style.background=color;
+}
+
+// let result2 =document.getElementsByClassName("btn2")
+// function btncolor2 ()
+// {
+//     document.body.style.background="green";
+// }
+
+
+// let result3 =document.getElementsByClassName("btn3")
+// function btncolor3 ()
+// {
+//     document.body.style.background="blue";
+// }
+
+// let result4 =document.getElementsByClassName("btn4")
+// function btncolor4 ()
+// {
+//     document.body.style.background="yellow";
+// }
+
+
+// let res5 =document.getElementsByClassName("btn5")
+// function btncolor5 ()
+// {
+//     document.body.style.background="blueviolet";
+// }
+
+
